3d39b467febc68514709c93c5e914831df184d55
Edwin Siebel <edwin@yard.nl>
2018-07-03 16:01:03 +0200

    Update: tests

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -2,20 +2,7 @@
 
 /**
  * PHPUnit bootstrap file
- *
- * @package OWC_OpenPub_Base
  */
-
-//$_tests_dir = getenv( 'WP_TESTS_DIR' );
-//if ( ! $_tests_dir ) {
-//	$_tests_dir = '/tmp/wordpress-tests-lib';
-//}
-//
-//// Give access to tests_add_filter() function.
-//require_once $_tests_dir . '/includes/functions.php';
-//
-//// Start up the WP testing environment.
-//require $_tests_dir . '/includes/bootstrap.php';
 
 /**
  * Load dependencies with Composer autoloader.
@@ -23,17 +10,40 @@
 require __DIR__ . '/../../vendor/autoload.php';
 
 define('WP_PLUGIN_DIR', __DIR__);
+define('WP_DEBUG', false);
 
 /**
  * Bootstrap WordPress Mock.
  */
-\WP_Mock::setUsePatchwork( true );
+\WP_Mock::setUsePatchwork(true);
 \WP_Mock::bootstrap();
 
-$GLOBALS['openpub-base'] = array(
-	'active_plugins' => array( 'openpub-base/openpub-base.php' ),
-);
+$GLOBALS['openpub-base'] = [
+    'active_plugins' => ['openpub-base/openpub-base.php'],
+];
 
-class WP_CLI {
-	public static function add_command() {}
+class WP_CLI
+{
+    public static function add_command()
+    {
+    }
+}
+
+if ( ! function_exists('get_echo') ) {
+
+    /**
+     * Capture the echo of a callable function.
+     *
+     * @param       $callable
+     * @param array $args
+     *
+     * @return string
+     */
+    function get_echo($callable, $args = [])
+    {
+        ob_start();
+        call_user_func_array($callable, $args);
+
+        return ob_get_clean();
+    }
 }
