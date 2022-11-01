6514a696d881fcc54e4b0853d5e9d4af08e01452
Simon <simon@yard.nl>
2022-11-01 14:24:14 +0100

    (chore): upgrade elasticpress

--- src/Base/Foundation/Plugin.php
+++ src/Base/Foundation/Plugin.php
@@ -24,7 +24,7 @@ class Plugin
      *
      * @var string VERSION
      */
-    public const VERSION = '2.1.0';
+    public const VERSION = '2.1.1';
 
     /**
      * Path to the root of the plugin.
