6aeffccfad06b4393c56ffc183d17b8e4864bcfa
Sander de Kroon <sanderdekroon@live.nl>
2023-02-28 15:19:35 +0100

    chore: update changelog and bump version to 2.3.0

--- src/Base/Foundation/Plugin.php
+++ src/Base/Foundation/Plugin.php
@@ -26,7 +26,7 @@ class Plugin
      *
      * @var string VERSION
      */
-    public const VERSION = '2.2.2';
+    public const VERSION = '2.3.0';
 
     /**
      * Path to the root of the plugin.
