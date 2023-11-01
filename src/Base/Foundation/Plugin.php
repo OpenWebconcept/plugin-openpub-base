da6a12e414bf34f57726923025fce0892a64b98d
Mike van den Hoek <mike@yard.nl>
2023-11-01 16:28:39 +0100

    (refactor): show taxonomy 'openpub-show-on' in rest

--- src/Base/Foundation/Plugin.php
+++ src/Base/Foundation/Plugin.php
@@ -26,7 +26,7 @@ class Plugin
      *
      * @var string VERSION
      */
-    public const VERSION = '2.3.9';
+    public const VERSION = '2.3.10';
 
     /**
      * Path to the root of the plugin.
